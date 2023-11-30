f9d5f1e55f4cffe2f016462ea5731f2fa7a20328
Suwannee <suwannee.thongtha@dexon-technology.com>
2023-11-30 13:52:49 +0700

    first commit

---
+++ hello-nextjs/pages/about.jsx
@@ -0,0 +1,8 @@
+// pages/index.js
+export default function About(){
+    return (
+        <div>
+        <h1>About 55</h1>
+      </div>
+    )
+}
---
+++ hello-nextjs/pages/contact.jsx
@@ -0,0 +1,8 @@
+// pages/index.js
+export default function contact(){
+  return (
+      <div>
+      <h1>con 55</h1>
+    </div>
+  )
+}
